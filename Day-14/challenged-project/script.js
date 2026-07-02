b5ceed7f785903de21f20b2f250601829c3876ea
Pratik Uchitkar <uchitkarpratik02@gmail.com>
2026-07-03 00:16:37 +0530

    Add files via upload

---
+++ Day-14/challenged-project/script.js
@@ -0,0 +1,20 @@
+console.log("Hello!"); 
+let PersonalInformation={
+    Name      : "Pratik Uchitkar",
+    Education : "B.Tech CSE",
+    Learning  : "MERN Stack",
+};
+let Skills={
+    Technologies:"HTML CSS Bootstrap JavaScript"
+};
+let Goals={
+    goal:"Become a Full Stack Developer",
+};
+console.log(PersonalInformation); 
+console.log(Skills);
+console.log(Goals); 
+
+
+
+
+
