b4501923e3e552e8994ff84e79b5cab59d7e65a9
Pratik Uchitkar <uchitkarpratik02@gmail.com>
2026-07-02 20:38:13 +0530

    Add files via upload

---
+++ Day-14/exercise/variables.js
@@ -0,0 +1,11 @@
+let firstName = "Pratik";
+let lastName = "Uchitkar";
+let city = "Nagpur";
+let country="India";
+let profession = "B.tech"
+console.log(firstName)
+console.log(lastName)
+console.log(city)
+console.log(country)
+console.log(profession)
+
