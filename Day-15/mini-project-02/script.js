ec97363ef29ca87976782b2f7d518621f68a5166
Pratik Uchitkar <uchitkarpratik02@gmail.com>
2026-07-03 22:28:28 +0530

    Add files via upload

---
+++ Day-15/mini-project-02/script.js
@@ -0,0 +1,28 @@
+let marks = prompt("Enter Your Marks");
+
+
+if (marks >= 80){
+    alert("A+ Congratulations")
+}
+else if (marks >= 70){
+    alert("A Congratulations")
+}
+else if (marks >= 60){
+    alert("B Congratulations")
+}
+else if (marks >= 45){
+    alert("Pass Congratulations")
+}
+else if (marks < 45){
+    alert("Better Luck Next Time")
+}
+else{alert("Invalid")};
+
+
+
+
+
+
+
+
+
