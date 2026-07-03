fcd9e6af81d8cfb5244b58831597315cb92ac2f7
Pratik Uchitkar <uchitkarpratik02@gmail.com>
2026-07-03 22:27:37 +0530

    Add files via upload

---
+++ Day-15/mini-project-01/script.js
@@ -0,0 +1,7 @@
+let fname=prompt("Enter Your name");
+let age=prompt("Enter Your Age");
+
+if (age >= 18){
+    alert(`hello ${fname} You are eligible to vote.`)
+}
+else{alert(`Sorry ${fname} You are not eligible.`)};
