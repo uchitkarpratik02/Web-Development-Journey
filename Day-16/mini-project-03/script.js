20c6449e876935a6fcc7dbf620bd27893962d2a5
Pratik Uchitkar <uchitkarpratik02@gmail.com>
2026-07-05 15:14:40 +0530

    Add files via upload

---
+++ Day-16/mini-project-03/script.js
@@ -0,0 +1,13 @@
+const movies = [
+  "Inception",
+  "The Matrix",
+  "Interstellar",
+  "The Dark Knight",
+  "Avatar"
+];
+movies.splice(4,1,"HM");//Add movie Delete movie //Replace movie
+console.log(movies);
+movies.reverse();//Reverse list
+console.log(movies);
+movies.sort();//Print numbered list
+console.log(movies);
