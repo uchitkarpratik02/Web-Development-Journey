bb587f20c0ce50e0ac3ab775e2ea6b3d0bf62bfc
Pratik Uchitkar <uchitkarpratik02@gmail.com>
2026-07-05 15:13:36 +0530

    Add files via upload

---
+++ Day-16/mini-project-02/script.js
@@ -0,0 +1,6 @@
+let cart=["Milk","Bread","Rice"];
+cart.push("butter");  //Add Item
+cart.pop();//Remove Item
+cart.indexOf("Rice");   //Search Item
+cart.reverse();  //Reverse Cart
+cart.sort();  //Sort Cart
