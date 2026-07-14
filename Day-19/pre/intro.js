363bd55149e9ad9b54c29ef66babf138d65f332f
Pratik Uchitkar <uchitkarpratik02@gmail.com>
2026-07-14 13:21:33 +0530

    Update intro.js

--- Day-19/pre/intro.js
+++ Day-19/pre/intro.js
@@ -1,4 +1,11 @@
-function Intro(Name,Age,Hometown){
-    console.log(`Hello, my name is ${Name}, I am ${Age}, and I have come all the way from ${Hometown}.`);
+function intro(Full_name,Age,Hobby,Goal){
+    alert(`
+        My name is ${Full_name}, and I am ${Age} years old. I enjoy reading books and learning new things. My favorite hobby is ${Hobby}, and I do it in my free time. In the future, I hope to become ${Goal}.
+        `)
 }
-Intro("Pratik",23,"Nagpur");
+intro(
+    prompt("Enter Your Full Name"),
+    prompt("Enter Your Age"),
+    prompt("Enter Your Hobby"),
+    prompt("Enter Your Goal")
+)
