1f1490dcf82a126e84609366969049ee194d1dbc
Pratik Uchitkar <uchitkarpratik02@gmail.com>
2026-07-17 21:44:12 +0530

    Add files via upload

---
+++ Day-22/Exercise/query-selector.js
@@ -0,0 +1,8 @@
+// First button
+let btn = document.querySelector("button");
+// First paragraph
+let para = document.querySelector("p");
+// All cards
+let card = document.querySelectorAll(".card")
+// All list items
+let li = document.querySelectorAll("li")
---
+++ Day-22/Exercise/selecting-id.js
@@ -0,0 +1,5 @@
+let title = document.getElementById("title");
+undefined
+title.textContent = "Watch My DOM Class";
+title.style.color="red";
+title.style.fontSize="50px";
