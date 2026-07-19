bf0e32bce36d324fe825378308e3e95d6da3cea9
Pratik Uchitkar <uchitkarpratik02@gmail.com>
2026-07-19 14:47:57 +0530

    Add files via upload

---
+++ Day-22/Exercise/attributes.js
@@ -0,0 +1,4 @@
+let img = document.getElementsByTagName("img");
+img[0].setAttribute("src", "image.png");
+img[0].setAttribute("alt", "Tony Stark Photo");
+img[0].setAttribute("title", "Hover tooltip text");
