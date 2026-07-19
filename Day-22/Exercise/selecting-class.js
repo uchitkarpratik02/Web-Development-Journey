5462eac228ba389796b46ff678ef43c9543420de
Pratik Uchitkar <uchitkarpratik02@gmail.com>
2026-07-19 17:53:49 +0530

    Delete Day-22/Exercise directory

--- Day-22/Exercise/selecting-class.js
+++
@@ -1,6 +0,0 @@
-let cart = document.getElementsByClassName("card");
-for(let i=0;i<cart.length;i++){
-    cart[i].style.backgroundColor = "red";
-    cart[i].style.color = "rgb(255,255,255)";
-    cart[i].style.border = "2px solid aqua";
-}
--- Day-22/Exercise/selecting-id.js
+++
@@ -1,5 +0,0 @@
-let title = document.getElementById("title");
-undefined
-title.textContent = "Watch My DOM Class";
-title.style.color="red";
-title.style.fontSize="50px";
